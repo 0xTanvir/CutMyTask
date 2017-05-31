09000110a80377258c3bb76fbbe33d236eb50e1d
Tanvir Rahman <dhurfaltu@gmail.com>
2017-05-31 10:44:56 +0600

    Add Logo and some very basic layout

--- app/src/main/java/com/bachelorshelter/tanvir/cutmytask/SplashActivity.java
+++ app/src/main/java/com/bachelorshelter/tanvir/cutmytask/SplashActivity.java
@@ -1,13 +1,24 @@
 package com.bachelorshelter.tanvir.cutmytask;
 
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static boolean isShowPageStart = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        if (isShowPageStart){
+            SystemClock.sleep(1000);
+            isShowPageStart = false;
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
